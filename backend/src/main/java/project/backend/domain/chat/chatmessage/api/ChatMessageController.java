1b5d6a306c3ebf650300356dc952ef5897604408
zie-ning <jeeun03@gmail.com>
2025-05-09 14:35:09 +0900

    init: 웹소켓 config 추가

--- backend/src/main/java/project/backend/domain/chat/chatmessage/api/ChatMessageController.java
+++ backend/src/main/java/project/backend/domain/chat/chatmessage/api/ChatMessageController.java
@@ -1,5 +1,27 @@
 package project.backend.domain.chat.chatmessage.api;
 
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import project.backend.domain.chat.chatmessage.app.ChatMessageService;
+import project.backend.domain.chat.chatmessage.dto.ChatMessageRequest;
+
+@Controller
+@RequiredArgsConstructor
 public class ChatMessageController {
 
+    private final ChatMessageService chatMessageService;
+
+    @MessageMapping("/send-message/{roomId}") //클라이언트가 메세지를 보낼 경로
+    @SendTo("/topic/public") //브로드캐스트할 경로
+    public ChatMessageRequest sendMessage(@DestinationVariable Long roomId,
+        @Payload ChatMessageRequest request,
+        Principal principal) {
+        chatMessageService.save(roomId, request, principal.getName());
+        return request;
+    }
 }
--- backend/src/main/java/project/backend/domain/chat/chatmessage/app/ChatMessageService.java
+++ backend/src/main/java/project/backend/domain/chat/chatmessage/app/ChatMessageService.java
@@ -1,5 +1,14 @@
 package project.backend.domain.chat.chatmessage.app;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.backend.domain.chat.chatmessage.dto.ChatMessageRequest;
+
+@Service
+@RequiredArgsConstructor
 public class ChatMessageService {
 
+    public void save(Long roomId, ChatMessageRequest request, String email) {
+
+    }
 }
