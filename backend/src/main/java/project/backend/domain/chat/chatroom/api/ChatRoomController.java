c477a9066b4179dad38bc12d2ce6c572874e4d2a
limchangin <dlackddls5@naver.com>
2025-05-11 18:23:23 +0900

    Feat: 초대 url 생성기능구현

--- backend/src/main/java/project/backend/domain/chat/chatroom/api/ChatRoomController.java
+++ backend/src/main/java/project/backend/domain/chat/chatroom/api/ChatRoomController.java
@@ -1,14 +1,18 @@
 package project.backend.domain.chat.chatroom.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import project.backend.domain.chat.chatroom.app.ChatRoomService;
 import project.backend.domain.chat.chatroom.dto.ChatRoomRequest;
 import project.backend.domain.chat.chatroom.dto.ChatRoomResponse;
+import project.backend.domain.chat.chatroom.dto.InviteCodeResponse;
 
 @RestController
 @RequestMapping("/api/chat-rooms")
@@ -19,10 +23,20 @@ public class ChatRoomController {
 
 	@PostMapping
 	public ChatRoomResponse create(@RequestBody ChatRoomRequest request) {
+		return chatRoomService.createChatRoom(request, request.getOwnerId());
+	}
 
-		ChatRoomResponse response = chatRoomService.createChatRoom(request, request.getOwnerId());
+	@GetMapping("/{roomId}/invite")
+	public InviteCodeResponse getInviteUrl(@PathVariable Long roomId) {
+		String inviteCode = chatRoomService.getInviteCode(roomId);
 
-		return response;
+		String inviteUrl = ServletUriComponentsBuilder
+			.fromCurrentContextPath()
+			.path("/api/chat-rooms/join")
+			.path(inviteCode)
+			.toUriString();
+
+		return new InviteCodeResponse(inviteUrl);
 	}
 
 }
