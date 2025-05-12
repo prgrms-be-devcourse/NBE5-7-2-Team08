be80f41423db32b535070841a1eeacca2993f56c
limkanghyun <limkh06910691@gmail.com>
2025-05-12 12:32:34 +0900

    Feat: ImageFile 엔티티, type 추가

--- backend/src/main/java/project/backend/domain/imagefile/ImageFile.java
+++ backend/src/main/java/project/backend/domain/imagefile/ImageFile.java
@@ -1,5 +1,30 @@
 package project.backend.domain.imagefile;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ImageFile {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String storeFileName;
+
+	private String uploadFileName;
+
+	@Enumerated(EnumType.STRING)
+	private ImageType image_type;
+
 }
+
---
+++ backend/src/main/java/project/backend/domain/imagefile/ImageType.java
@@ -0,0 +1,9 @@
+package project.backend.domain.imagefile;
+
+public enum ImageType {
+
+	PROFILE_IMAGE,
+	CHAT_IMAGE
+
+}
+
