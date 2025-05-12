95aafe6a39a721a42504f5eeb8acc6235699605f
zie-ning <jeeun03@gmail.com>
2025-05-12 11:24:55 +0900

    Refactor: 예외핸들러 import clear

--- backend/src/main/java/project/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/project/backend/global/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package project.backend.global.exception;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import project.backend.global.exception.ex.BaseException;
 
 @RestControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
