70077112dabb9a9fe29c3ae17850a485b4f82aa2
limchangin <dlackddls5@naver.com>
2025-05-16 00:00:27 +0900

    Fix: 오류 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,10 @@ import Home from './pages/Home'
 import BlankRoom from './pages/BlankRoom'
 import Login from "./pages/login-form"
 import Signup from "./pages/signup"
-<<<<<<< HEAD
-=======
-import JoinPage from './pages/JoinPage';
 import MyPage from "./pages/profile"
 import EditProfilePage from "./pages/editprofile"
 
->>>>>>> 6d0e88374524ec70cd0c060c30e04506ef352fd8
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -24,12 +21,10 @@ function App() {
         <Route path="/blank" element={<BlankRoom />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
-=======
         <Route path="/myprofile" element={<MyPage />} />
         <Route path="/myprofile/edit" element={<EditProfilePage />} />
-        <Route path="/join" element={<JoinPage />} />
->>>>>>> 6d0e88374524ec70cd0c060c30e04506ef352fd8
+ 
+
       </Routes>
     </BrowserRouter>
   );
