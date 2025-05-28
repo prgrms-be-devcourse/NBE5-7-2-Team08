96fc4fa5f638bd4a88ea379a8f0aebfed03322f1
limchangin <dlackddls5@naver.com>
2025-05-29 00:23:36 +0900

    Chore: 주석지움

--- backend/src/main/java/project/backend/domain/chat/chatmessage/dao/ChatMessageRepository.java
+++ backend/src/main/java/project/backend/domain/chat/chatmessage/dao/ChatMessageRepository.java
@@ -10,18 +10,6 @@ import org.springframework.data.repository.query.Param;
 import project.backend.domain.chat.chatmessage.entity.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-
-//	@Query(value = """
-//		select m.room_id
-//		from chat_message m
-//		join chat_participant cp on cp.room_id = m.room_id
-//		join member mb on cp.member_id = mb.member_id
-//		where mb.email = :email
-//		order by m.send_at desc
-//		limit 1
-//		""", nativeQuery = true)
-//	Optional<Long> findMostRecentRoomIdByMemberEmail(@Param("email") String email);
-
 	@Query("""
         SELECT cm.chatRoom.id 
         FROM ChatMessage cm
